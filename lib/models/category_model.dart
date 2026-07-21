def39476b54b0b1c1c17d809d99be9741a4474ba
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-21 20:22:15 +0200

    Create CategoryModel class with properties

---
+++ lib/models/category_model.dart
@@ -0,0 +1,11 @@
+class CategoryModel {
+  final String name;
+  final String image;
+  final String id;
+
+  const CategoryModel({
+    required this.name,
+    required this.image,
+    required this.id,
+  });
+}
