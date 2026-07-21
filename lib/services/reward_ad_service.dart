ae3253071acd754454d9cee53feb8cc92b205229
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-21 21:29:24 +0200

    Create RewardAdService for handling rewarded ads
    Implement RewardAdService with a placeholder method for showing ads.

---
+++ lib/services/reward_ad_service.dart
@@ -0,0 +1,19 @@
+class RewardAdService {
+
+
+  // مؤقتًا يرجع نجاح المشاهدة
+  // لاحقًا نربطه مع AdMob Rewarded Ads
+
+
+  static Future<bool> showContinueAd() async {
+
+
+    // هنا سيأتي إعلان AdMob لاحقًا
+
+
+    return true;
+
+  }
+
+
+}
