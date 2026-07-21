9301b4fc776067493539c4bbf44803c4c2c302d2
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-21 21:19:40 +0200

    Create game_result_model.dart

---
+++ lib/models/game_result_model.dart
@@ -0,0 +1,21 @@
+class GameResultModel {
+
+  final int stars;
+
+  final int moves;
+
+  final Duration time;
+
+
+  const GameResultModel({
+
+    required this.stars,
+
+    required this.moves,
+
+    required this.time,
+
+  });
+
+
+}
