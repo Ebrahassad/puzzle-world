6db227c940db2afa998c0919bd94b057e6cc29f1
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-22 13:14:09 +0200

    Create puzzle_settings_service.dart

---
+++ lib/features/puzzle/services/puzzle_settings_service.dart
@@ -0,0 +1,188 @@
+import '../managers/puzzle_progress_manager.dart';
+
+
+
+class PuzzleSettingsService {
+
+
+  static Future<Map<String, dynamic>>
+
+  getSettings() async {
+
+
+
+    final sound =
+
+    await PuzzleProgressManager
+
+        .isSoundEnabled();
+
+
+
+
+
+    final darkMode =
+
+    await PuzzleProgressManager
+
+        .isDarkMode();
+
+
+
+
+
+    final vibration =
+
+    await PuzzleProgressManager
+
+        .isVibrationEnabled();
+
+
+
+
+
+    return {
+
+
+
+      "sound": sound,
+
+
+      "darkMode": darkMode,
+
+
+      "vibration": vibration,
+
+
+
+    };
+
+
+  }
+
+
+
+
+
+
+
+
+  static Future<void> setSound(
+
+      bool value,
+
+      ) async {
+
+
+
+    await PuzzleProgressManager
+
+        .saveSoundEnabled(
+
+      value,
+
+    );
+
+
+  }
+
+
+
+
+
+
+
+
+  static Future<void> setDarkMode(
+
+      bool value,
+
+      ) async {
+
+
+
+    await PuzzleProgressManager
+
+        .saveDarkMode(
+
+      value,
+
+    );
+
+
+  }
+
+
+
+
+
+
+
+
+  static Future<void> setVibration(
+
+      bool value,
+
+      ) async {
+
+
+
+    await PuzzleProgressManager
+
+        .saveVibrationEnabled(
+
+      value,
+
+    );
+
+
+  }
+
+
+
+
+
+
+
+
+  static Future<void> resetSettings() async {
+
+
+
+    await PuzzleProgressManager
+
+        .saveSoundEnabled(
+
+      true,
+
+    );
+
+
+
+
+
+    await PuzzleProgressManager
+
+        .saveDarkMode(
+
+      false,
+
+    );
+
+
+
+
+
+    await PuzzleProgressManager
+
+        .saveVibrationEnabled(
+
+      true,
+
+    );
+
+
+  }
+
+
+}
