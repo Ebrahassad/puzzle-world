5e9b413506aad6fd4ea04c0ddf2b13335c05830a
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-22 21:25:36 +0200

    Update puzzle_data.dart

--- lib/features/puzzle/data/puzzle_data.dart
+++ lib/features/puzzle/data/puzzle_data.dart
@@ -1,6 +1,7 @@
 import '../models/puzzle_model.dart';
 
 
+
 class PuzzleData {
 
 
@@ -14,9 +15,20 @@ class PuzzleData {
 
       title: "عالم الحيوانات 🐻",
 
-      image: "assets/images/Puzzle/animals.png",
+      image:
+      "assets/images/Puzzle/animals.png",
+
+      description:
+      "اكتشف الحيوانات الجميلة وحل البازل الخاص بها",
+
+      totalLevels: 10,
+
+      unlocked: true,
+
+      requiredStars: 0,
 
     ),
+
 
 
 
@@ -27,9 +39,20 @@ class PuzzleData {
 
       title: "عالم الطبيعة 🌳",
 
-      image: "assets/images/Puzzle/nature.png",
+      image:
+      "assets/images/Puzzle/nature.png",
+
+      description:
+      "مناظر طبيعية وألغاز ممتعة",
+
+      totalLevels: 10,
+
+      unlocked: false,
+
+      requiredStars: 20,
 
     ),
+
 
 
 
@@ -40,9 +63,20 @@ class PuzzleData {
 
       title: "عالم السيارات 🚗",
 
-      image: "assets/images/Puzzle/vehicles.png",
+      image:
+      "assets/images/Puzzle/vehicles.png",
+
+      description:
+      "سيارات ومركبات بطريقة تعليمية",
+
+      totalLevels: 10,
+
+      unlocked: false,
+
+      requiredStars: 50,
 
     ),
+
 
 
 
@@ -53,7 +87,17 @@ class PuzzleData {
 
       title: "عالم الفواكه 🍎",
 
-      image: "assets/images/Puzzle/fruits.png",
+      image:
+      "assets/images/Puzzle/fruits.png",
+
+      description:
+      "تعلم الفواكه من خلال اللعب",
+
+      totalLevels: 10,
+
+      unlocked: false,
+
+      requiredStars: 80,
 
     ),
 
@@ -61,6 +105,37 @@ class PuzzleData {
 
   ];
 
+
+
+
+
+  // جلب عالم بواسطة المعرف
+
+  static PuzzleModel? getById(
+      String id,
+      ){
+
+    try {
+
+      return puzzles.firstWhere(
+            (world) => world.id == id,
+      );
+
+    } catch (_) {
+
+      return null;
+
+    }
+
+  }
+
+
+
+
+
+  // عدد العوالم
+
+  static int get count => puzzles.length;
 
 
 }
