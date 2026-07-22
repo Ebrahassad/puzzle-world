ffef129ba45e22beb03a4aa17ec780166beea0fd
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-22 19:29:42 +0200

    Update puzzle_progress_manager.dart

--- lib/features/puzzle/managers/puzzle_progress_manager.dart
+++ lib/features/puzzle/managers/puzzle_progress_manager.dart
@@ -122,13 +122,18 @@ class PuzzleProgressManager {
   }
 
   static Future<void> saveStars(int stars) async {
-    final prefs = await _prefs;
+  final prefs = await _prefs;
 
+  final current =
+      prefs.getInt(starsKey) ?? 0;
+
+  if (stars > current) {
     await prefs.setInt(
       starsKey,
       stars,
     );
   }
+}
 
   static Future<void> addStars(int amount) async {
     final prefs = await _prefs;
@@ -173,10 +178,16 @@ await prefs.setInt(
     final current =
         prefs.getInt(coinsKey) ?? 0;
 
-    await prefs.setInt(
-      coinsKey,
-      current + amount,
-    );
+    int value = current + amount;
+
+if(value < 0){
+  value = 0;
+}
+
+await prefs.setInt(
+  coinsKey,
+  value,
+);
   }
 
   //==================================================
@@ -522,61 +533,11 @@ await prefs.setInt(
 
     await addGamePlayed();
 
-    await addStars(stars);
+    
   }
 
 
-//==================================================
-// حفظ آخر بازل
-//==================================================
 
-static Future<void> saveLastPuzzle(
-  String worldId,
-  String levelId,
-) async {
-
-  await saveLastGame(
-    "$worldId-$levelId",
-  );
-
-}
-
-static Future<Map<String,dynamic>?>
-getLastPlayed() async {
-
-
-  final value =
-      await PuzzleProgressManager.getLastGame();
-
-
-  if(value == null){
-
-    return null;
-
-  }
-
-
-  final parts =
-      value.split("-");
-
-
-  if(parts.length != 2){
-
-    return null;
-
-  }
-
-
-  return {
-
-    "worldId": parts[0],
-
-    "levelId": parts[1],
-
-  };
-
-
-}
 
   //==================================================
   // إعادة تعيين الإحصائيات فقط
