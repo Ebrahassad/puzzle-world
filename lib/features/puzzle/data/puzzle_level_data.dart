bf0ab00965c6485a36fae0415dfadc08d7371952
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-22 04:00:42 +0200

    Create puzzle_level_data.dart

---
+++ lib/features/puzzle/data/puzzle_level_data.dart
@@ -0,0 +1,181 @@
+import '../models/puzzle_level_model.dart';
+
+
+
+class PuzzleLevelData {
+
+
+
+  static const Map<String, List<PuzzleLevelModel>> levels = {
+
+
+
+    "animals":[
+
+
+      PuzzleLevelModel(
+
+        id:"level_1",
+
+        gridSize:3,
+
+        requiredStars:0,
+
+        unlocked:true,
+
+      ),
+
+
+
+      PuzzleLevelModel(
+
+        id:"level_2",
+
+        gridSize:4,
+
+        requiredStars:3,
+
+      ),
+
+
+
+      PuzzleLevelModel(
+
+        id:"level_3",
+
+        gridSize:5,
+
+        requiredStars:6,
+
+      ),
+
+
+
+      PuzzleLevelModel(
+
+        id:"level_4",
+
+        gridSize:6,
+
+        requiredStars:9,
+
+      ),
+
+
+    ],
+
+
+
+
+
+
+    "nature":[
+
+
+      PuzzleLevelModel(
+
+        id:"level_1",
+
+        gridSize:3,
+
+        requiredStars:0,
+
+        unlocked:true,
+
+      ),
+
+
+
+      PuzzleLevelModel(
+
+        id:"level_2",
+
+        gridSize:4,
+
+        requiredStars:3,
+
+      ),
+
+
+
+      PuzzleLevelModel(
+
+        id:"level_3",
+
+        gridSize:5,
+
+        requiredStars:6,
+
+      ),
+
+
+    ],
+
+
+
+
+
+
+    "vehicles":[
+
+
+      PuzzleLevelModel(
+
+        id:"level_1",
+
+        gridSize:3,
+
+        requiredStars:0,
+
+        unlocked:true,
+
+      ),
+
+
+
+      PuzzleLevelModel(
+
+        id:"level_2",
+
+        gridSize:5,
+
+        requiredStars:3,
+
+      ),
+
+
+
+      PuzzleLevelModel(
+
+        id:"level_3",
+
+        gridSize:6,
+
+        requiredStars:6,
+
+      ),
+
+
+    ],
+
+
+
+  };
+
+
+
+
+
+  static List<PuzzleLevelModel> getLevels(
+
+      String puzzleId,
+
+      ){
+
+    return levels[puzzleId] ?? [];
+
+  }
+
+
+
+}
