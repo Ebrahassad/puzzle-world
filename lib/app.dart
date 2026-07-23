b9c5a7ef187d9b309622d4dd25deea7b2ef6a702
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-23 05:38:04 +0200

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'screens/home_screen.dart';
+import 'features/puzzle/screens/puzzle_home_screen.dart';
 
 
 class PuzzleWorldApp extends StatelessWidget {
 
-  const PuzzleWorldApp({super.key});
+  const PuzzleWorldApp({
+    super.key,
+  });
 
 
   @override
   Widget build(BuildContext context) {
 
-
     return MaterialApp(
 
       debugShowCheckedModeBanner:false,
 
-
       title:'Puzzle World',
-
 
       theme: ThemeData(
 
@@ -29,7 +28,7 @@ class PuzzleWorldApp extends StatelessWidget {
       ),
 
 
-      home:const HomeScreen(),
+      home:const PuzzleHomeScreen(),
 
     );
 
