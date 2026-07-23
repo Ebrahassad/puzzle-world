626d04a7a3902e481bdb60ca2617831f6792f369
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-23 04:05:52 +0200

    Update puzzle_session_service.dart

--- lib/features/puzzle/services/puzzle_session_service.dart
+++ lib/features/puzzle/services/puzzle_session_service.dart
@@ -1,178 +1,98 @@
 import '../managers/puzzle_progress_manager.dart';
 
-
-
 class PuzzleSessionService {
-
 
   static DateTime? _startTime;
 
-
   static String? _worldId;
-
 
   static int? _level;
 
 
-
-
-
-
-
-
   static void startSession({
-
     required String worldId,
-
     required int level,
-
   }) {
-
-
 
     _worldId = worldId;
 
-
     _level = level;
-
 
     _startTime = DateTime.now();
 
-
   }
-
-
-
-
-
 
 
 
   static Future<void> endSession({
-
     int moves = 0,
-
   }) async {
 
-
-
     if(_startTime == null){
-
       return;
-
     }
 
 
-
-
-
     final duration =
-
-    DateTime.now()
-
-        .difference(
-
-      _startTime!,
-
-    );
+        DateTime.now()
+            .difference(_startTime!);
 
 
+    try {
+
+      await PuzzleProgressManager
+          .addPlayTime(
+        duration.inSeconds,
+      );
 
 
-
-    await PuzzleProgressManager
-
-        .addPlayTime(
-
-      duration.inSeconds,
-
-    );
+      await PuzzleProgressManager
+          .addTotalMoves(
+        moves,
+      );
 
 
+    } finally {
 
 
+      _startTime = null;
 
-    await PuzzleProgressManager
+      _worldId = null;
 
-        .addTotalMoves(
-
-      moves,
-
-    );
+      _level = null;
 
 
-
-
-
-    _startTime = null;
-
-
-    _worldId = null;
-
-
-    _level = null;
-
+    }
 
   }
-
-
-
-
-
 
 
 
   static bool isActive(){
 
-
-
     return _startTime != null;
 
-
   }
-
-
-
-
-
 
 
 
   static String? getWorld(){
 
-
-
     return _worldId;
 
-
   }
-
-
-
-
-
 
 
 
   static int? getLevel(){
 
-
-
     return _level;
-
 
   }
 
 
 
-
-
-
-
-
   static Duration getDuration(){
-
-
 
     if(_startTime == null){
 
@@ -181,17 +101,10 @@ class PuzzleSessionService {
     }
 
 
-
-
-
     return DateTime.now()
-
         .difference(
-
       _startTime!,
-
     );
-
 
   }
 
