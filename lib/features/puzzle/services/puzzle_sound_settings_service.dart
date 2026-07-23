9fc2f6d98ab79928ea0bc3e9e9db7ff55aacc09f
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-23 03:57:52 +0200

    Update puzzle_sound_settings_service.dart

--- lib/features/puzzle/services/puzzle_sound_settings_service.dart
+++ lib/features/puzzle/services/puzzle_sound_settings_service.dart
@@ -1,114 +1,62 @@
 import '../managers/puzzle_progress_manager.dart';
 
 
-
 class PuzzleSoundSettingsService {
+
+
+  const PuzzleSoundSettingsService._();
 
 
   static Future<bool> isEnabled() async {
 
 
     return await PuzzleProgressManager
-
         .isSoundEnabled();
 
 
   }
 
 
-
-
-
-
-
-
   static Future<void> enable() async {
 
 
     await PuzzleProgressManager
-
-        .saveSoundEnabled(
-
-      true,
-
-    );
+        .saveSoundEnabled(true);
 
 
   }
-
-
-
-
-
-
 
 
   static Future<void> disable() async {
 
 
     await PuzzleProgressManager
-
-        .saveSoundEnabled(
-
-      false,
-
-    );
+        .saveSoundEnabled(false);
 
 
   }
-
-
-
-
-
-
 
 
   static Future<void> toggle() async {
 
 
-
-    final current =
-
-    await isEnabled();
-
-
-
+    final current = await isEnabled();
 
 
     await PuzzleProgressManager
-
-        .saveSoundEnabled(
-
-      !current,
-
-    );
+        .saveSoundEnabled(!current);
 
 
   }
 
 
-
-
-
-
-
-
   static Future<void> setValue(
-
       bool value,
-
       ) async {
 
 
-
     await PuzzleProgressManager
-
-        .saveSoundEnabled(
-
-      value,
-
-    );
+        .saveSoundEnabled(value);
 
 
   }
