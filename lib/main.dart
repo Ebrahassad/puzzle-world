881077accbb387aa98bf32b26b57408c5e711f83
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-25 00:58:30 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'app.dart';
 
 void main() {
   runApp(
-    const ProviderScope(
-      child: PuzzleWorldApp(),
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("TEST"),
+        ),
+      ),
     ),
   );
 }
