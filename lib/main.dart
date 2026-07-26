fa9c5b0a398aa6d745f9d516761ac9f1dcf433d0
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-27 01:38:46 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+import 'core/theme/app_theme.dart';
 import 'splash_screen.dart';
 
+
+
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
+
 
   await MobileAds.instance.initialize();
 
+
   runApp(
+
     const PuzzleWorldApp(),
+
   );
+
 }
 
 
+
+
+
 class PuzzleWorldApp extends StatelessWidget {
-  const PuzzleWorldApp({super.key});
+
+
+  const PuzzleWorldApp({
+
+    super.key,
+
+  });
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
 
-      title: "Puzzle World",
 
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-        ),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner:false,
 
-      home: const SplashScreen(),
+
+
+      title:"Puzzle World",
+
+
+
+
+      theme:
+
+      AppTheme.lightTheme,
+
+
+
+
+
+      home:
+
+      const SplashScreen(),
+
+
+
     );
+
+
   }
+
+
 }
