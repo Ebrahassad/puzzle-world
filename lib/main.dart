3011f4c3dbc031613fe40be805cedb0f1194f229
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-26 03:00:27 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+import 'puzzle_world_app.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await MobileAds.instance.initialize();
 
   runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text("TEST"),
-        ),
-      ),
-    ),
+    const PuzzleWorldApp(),
   );
 }
