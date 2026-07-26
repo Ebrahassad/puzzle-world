40d44157308e02717c3c24979c5048d85548d84a
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-26 02:06:10 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text("TEST"),
-        ),
-      ),
+      home: TestPage(),
     ),
   );
+}
+
+class TestPage extends StatelessWidget {
+  const TestPage({super.key});
+
+  Future<void> testGoogleAds(BuildContext context) async {
+    await MobileAds.instance.initialize();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("Google Ads Initialized"),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => testGoogleAds(context),
+          child: const Text("Test Google Ads"),
+        ),
+      ),
+    );
+  }
 }
