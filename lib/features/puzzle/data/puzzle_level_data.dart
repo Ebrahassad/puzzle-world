426b2cc8dbcc660204cb8c51f86e7696c027af9d
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-27 01:07:05 +0200

    Update puzzle_level_data.dart

--- lib/features/puzzle/data/puzzle_level_data.dart
+++ lib/features/puzzle/data/puzzle_level_data.dart
@@ -136,3 +136,53 @@ class PuzzleLevelData {
         gridSize: 6,
         requiredStars: 9,
       ),
+
+      PuzzleLevelModel(
+        id: "level_5",
+        levelNumber: 5,
+        title: "السيارات الرياضية",
+        gridSize: 6,
+        requiredStars: 15,
+      ),
+
+      PuzzleLevelModel(
+        id: "level_6",
+        levelNumber: 6,
+        title: "طريق السرعة",
+        gridSize: 7,
+        requiredStars: 20,
+      ),
+
+      PuzzleLevelModel(
+        id: "level_7",
+        levelNumber: 7,
+        title: "سباق الأبطال",
+        gridSize: 7,
+        requiredStars: 30,
+      ),
+
+      PuzzleLevelModel(
+        id: "level_8",
+        levelNumber: 8,
+        title: "تحدي المدينة",
+        gridSize: 8,
+        requiredStars: 40,
+      ),
+
+      PuzzleLevelModel(
+        id: "level_9",
+        levelNumber: 9,
+        title: "أسطورة السيارات",
+        gridSize: 8,
+        requiredStars: 50,
+      ),
+
+      PuzzleLevelModel(
+        id: "level_10",
+        levelNumber: 10,
+        title: "السباق النهائي",
+        gridSize: 9,
+        requiredStars: 70,
+      ),
+
+    ],
