a68b2a8dedb5ba1dd585ae2b760207964a3560e4
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-27 02:38:00 +0200

    Update puzzle_level_data.dart

--- lib/features/puzzle/data/puzzle_level_data.dart
+++ lib/features/puzzle/data/puzzle_level_data.dart
@@ -4,18 +4,6 @@ import '../models/puzzle_level_model.dart';
 class PuzzleLevelData {
 
 
-  static String imagePath(
-      String puzzleId,
-      int level,
-      ) {
-
-    return "assets/images/Puzzle/$puzzleId/level_$level.png";
-
-  }
-
-
-
-
   static const Map<String, List<PuzzleLevelModel>> levels = {
 
 
@@ -35,7 +23,6 @@ class PuzzleLevelData {
         unlocked: true,
       ),
 
-
       PuzzleLevelModel(
         id: "level_2",
         levelNumber: 2,
@@ -44,7 +31,6 @@ class PuzzleLevelData {
         gridSize: 4,
         requiredStars: 3,
       ),
-
 
       PuzzleLevelModel(
         id: "level_3",
@@ -55,7 +41,6 @@ class PuzzleLevelData {
         requiredStars: 6,
       ),
 
-
       PuzzleLevelModel(
         id: "level_4",
         levelNumber: 4,
@@ -64,7 +49,6 @@ class PuzzleLevelData {
         gridSize: 6,
         requiredStars: 9,
       ),
-
 
       PuzzleLevelModel(
         id: "level_5",
@@ -75,7 +59,6 @@ class PuzzleLevelData {
         requiredStars: 15,
       ),
 
-
       PuzzleLevelModel(
         id: "level_6",
         levelNumber: 6,
@@ -84,7 +67,6 @@ class PuzzleLevelData {
         gridSize: 7,
         requiredStars: 20,
       ),
-
 
       PuzzleLevelModel(
         id: "level_7",
@@ -95,7 +77,6 @@ class PuzzleLevelData {
         requiredStars: 30,
       ),
 
-
       PuzzleLevelModel(
         id: "level_8",
         levelNumber: 8,
@@ -105,7 +86,6 @@ class PuzzleLevelData {
         requiredStars: 40,
       ),
 
-
       PuzzleLevelModel(
         id: "level_9",
         levelNumber: 9,
@@ -114,7 +94,6 @@ class PuzzleLevelData {
         gridSize: 8,
         requiredStars: 50,
       ),
-
 
       PuzzleLevelModel(
         id: "level_10",
@@ -126,6 +105,8 @@ class PuzzleLevelData {
       ),
 
     ],
+
+
 
     //==================================================
     // جزيرة السيارات
@@ -143,7 +124,6 @@ class PuzzleLevelData {
         unlocked: true,
       ),
 
-
       PuzzleLevelModel(
         id: "level_2",
         levelNumber: 2,
@@ -152,7 +132,6 @@ class PuzzleLevelData {
         gridSize: 4,
         requiredStars: 3,
       ),
-
 
       PuzzleLevelModel(
         id: "level_3",
@@ -163,7 +142,6 @@ class PuzzleLevelData {
         requiredStars: 6,
       ),
 
-
       PuzzleLevelModel(
         id: "level_4",
         levelNumber: 4,
@@ -172,7 +150,6 @@ class PuzzleLevelData {
         gridSize: 6,
         requiredStars: 9,
       ),
-
 
       PuzzleLevelModel(
         id: "level_5",
@@ -183,7 +160,6 @@ class PuzzleLevelData {
         requiredStars: 15,
       ),
 
-
       PuzzleLevelModel(
         id: "level_6",
         levelNumber: 6,
@@ -192,7 +168,6 @@ class PuzzleLevelData {
         gridSize: 7,
         requiredStars: 20,
       ),
-
 
       PuzzleLevelModel(
         id: "level_7",
@@ -203,7 +178,6 @@ class PuzzleLevelData {
         requiredStars: 30,
       ),
 
-
       PuzzleLevelModel(
         id: "level_8",
         levelNumber: 8,
@@ -213,7 +187,6 @@ class PuzzleLevelData {
         requiredStars: 40,
       ),
 
-
       PuzzleLevelModel(
         id: "level_9",
         levelNumber: 9,
@@ -222,7 +195,6 @@ class PuzzleLevelData {
         gridSize: 8,
         requiredStars: 50,
       ),
-
 
       PuzzleLevelModel(
         id: "level_10",
@@ -235,348 +207,312 @@ class PuzzleLevelData {
 
     ],
 
-
-
-
-
-    //==================================================
-    // جزيرة الفضاء
-    //==================================================
-
-    "space": [
-
-      PuzzleLevelModel(
-        id: "level_1",
-        levelNumber: 1,
-        title: "رحلة البداية",
-        image: "assets/images/Puzzle/space/level_1.png",
-        gridSize: 3,
-        requiredStars: 0,
-        unlocked: true,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_2",
-        levelNumber: 2,
-        title: "الكواكب",
-        image: "assets/images/Puzzle/space/level_2.png",
-        gridSize: 4,
-        requiredStars: 3,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_3",
-        levelNumber: 3,
-        title: "الصواريخ",
-        image: "assets/images/Puzzle/space/level_3.png",
-        gridSize: 5,
-        requiredStars: 6,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_4",
-        levelNumber: 4,
-        title: "أسرار الفضاء",
-        image: "assets/images/Puzzle/space/level_4.png",
-        gridSize: 6,
-        requiredStars: 9,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_5",
-        levelNumber: 5,
-        title: "المجرات",
-        image: "assets/images/Puzzle/space/level_5.png",
-        gridSize: 6,
-        requiredStars: 15,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_6",
-        levelNumber: 6,
-        title: "رحلة القمر",
-        image: "assets/images/Puzzle/space/level_6.png",
-        gridSize: 7,
-        requiredStars: 20,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_7",
-        levelNumber: 7,
-        title: "النجوم البعيدة",
-        image: "assets/images/Puzzle/space/level_7.png",
-        gridSize: 7,
-        requiredStars: 30,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_8",
-        levelNumber: 8,
-        title: "أسرار الكون",
-        image: "assets/images/Puzzle/space/level_8.png",
-        gridSize: 8,
-        requiredStars: 40,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_9",
-        levelNumber: 9,
-        title: "مغامرة الفضاء",
-        image: "assets/images/Puzzle/space/level_9.png",
-        gridSize: 8,
-        requiredStars: 50,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_10",
-        levelNumber: 10,
-        title: "نهاية الرحلة",
-        image: "assets/images/Puzzle/space/level_10.png",
-        gridSize: 9,
-        requiredStars: 70,
-      ),
-
-    ],
-
-    //==================================================
-    // جزيرة الطبيعة
-    //==================================================
-
-    "nature": [
-
-      PuzzleLevelModel(
-        id: "level_1",
-        levelNumber: 1,
-        title: "البداية",
-        image: "assets/images/Puzzle/nature/level_1.png",
-        gridSize: 3,
-        requiredStars: 0,
-        unlocked: true,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_2",
-        levelNumber: 2,
-        title: "الجبال والأنهار",
-        image: "assets/images/Puzzle/nature/level_2.png",
-        gridSize: 4,
-        requiredStars: 3,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_3",
-        levelNumber: 3,
-        title: "جمال الطبيعة",
-        image: "assets/images/Puzzle/nature/level_3.png",
-        gridSize: 5,
-        requiredStars: 6,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_4",
-        levelNumber: 4,
-        title: "تحدي الطبيعة",
-        image: "assets/images/Puzzle/nature/level_4.png",
-        gridSize: 6,
-        requiredStars: 9,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_5",
-        levelNumber: 5,
-        title: "الغابات الخضراء",
-        image: "assets/images/Puzzle/nature/level_5.png",
-        gridSize: 6,
-        requiredStars: 15,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_6",
-        levelNumber: 6,
-        title: "الشلالات",
-        image: "assets/images/Puzzle/nature/level_6.png",
-        gridSize: 7,
-        requiredStars: 20,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_7",
-        levelNumber: 7,
-        title: "عالم النباتات",
-        image: "assets/images/Puzzle/nature/level_7.png",
-        gridSize: 7,
-        requiredStars: 30,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_8",
-        levelNumber: 8,
-        title: "روعة الطبيعة",
-        image: "assets/images/Puzzle/nature/level_8.png",
-        gridSize: 8,
-        requiredStars: 40,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_9",
-        levelNumber: 9,
-        title: "سر الطبيعة",
-        image: "assets/images/Puzzle/nature/level_9.png",
-        gridSize: 8,
-        requiredStars: 50,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_10",
-        levelNumber: 10,
-        title: "التحدي النهائي",
-        image: "assets/images/Puzzle/nature/level_10.png",
-        gridSize: 9,
-        requiredStars: 70,
-      ),
-
-    ],
-
-
-
-
-    //==================================================
-    // جزيرة المعالم العالمية
-    //==================================================
-
-    "landmarks": [
-
-      PuzzleLevelModel(
-        id: "level_1",
-        levelNumber: 1,
-        title: "البداية",
-        image: "assets/images/Puzzle/landmarks/level_1.png",
-        gridSize: 3,
-        requiredStars: 0,
-        unlocked: true,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_2",
-        levelNumber: 2,
-        title: "معالم مشهورة",
-        image: "assets/images/Puzzle/landmarks/level_2.png",
-        gridSize: 4,
-        requiredStars: 3,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_3",
-        levelNumber: 3,
-        title: "حول العالم",
-        image: "assets/images/Puzzle/landmarks/level_3.png",
-        gridSize: 5,
-        requiredStars: 6,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_4",
-        levelNumber: 4,
-        title: "تحدي المعالم",
-        image: "assets/images/Puzzle/landmarks/level_4.png",
-        gridSize: 6,
-        requiredStars: 9,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_5",
-        levelNumber: 5,
-        title: "عجائب العالم",
-        image: "assets/images/Puzzle/landmarks/level_5.png",
-        gridSize: 6,
-        requiredStars: 15,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_6",
-        levelNumber: 6,
-        title: "مدن عالمية",
-        image: "assets/images/Puzzle/landmarks/level_6.png",
-        gridSize: 7,
-        requiredStars: 20,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_7",
-        levelNumber: 7,
-        title: "آثار قديمة",
-        image: "assets/images/Puzzle/landmarks/level_7.png",
-        gridSize: 7,
-        requiredStars: 30,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_8",
-        levelNumber: 8,
-        title: "رحلة حول العالم",
-        image: "assets/images/Puzzle/landmarks/level_8.png",
-        gridSize: 8,
-        requiredStars: 40,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_9",
-        levelNumber: 9,
-        title: "أشهر المعالم",
-        image: "assets/images/Puzzle/landmarks/level_9.png",
-        gridSize: 8,
-        requiredStars: 50,
-      ),
-
-
-      PuzzleLevelModel(
-        id: "level_10",
-        levelNumber: 10,
-        title: "النهائي العالمي",
-        image: "assets/images/Puzzle/landmarks/level_10.png",
-        gridSize: 9,
-        requiredStars: 70,
-      ),
-
-    ],
-
-    ],
+//==================================================
+// جزيرة الفضاء
+//==================================================
+
+"space": [
+
+  PuzzleLevelModel(
+    id: "level_1",
+    levelNumber: 1,
+    title: "رحلة البداية",
+    image: "assets/images/Puzzle/space/level_1.png",
+    gridSize: 3,
+    requiredStars: 0,
+    unlocked: true,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_2",
+    levelNumber: 2,
+    title: "الكواكب",
+    image: "assets/images/Puzzle/space/level_2.png",
+    gridSize: 4,
+    requiredStars: 3,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_3",
+    levelNumber: 3,
+    title: "الصواريخ",
+    image: "assets/images/Puzzle/space/level_3.png",
+    gridSize: 5,
+    requiredStars: 6,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_4",
+    levelNumber: 4,
+    title: "أسرار الفضاء",
+    image: "assets/images/Puzzle/space/level_4.png",
+    gridSize: 6,
+    requiredStars: 9,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_5",
+    levelNumber: 5,
+    title: "المجرات",
+    image: "assets/images/Puzzle/space/level_5.png",
+    gridSize: 6,
+    requiredStars: 15,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_6",
+    levelNumber: 6,
+    title: "رحلة القمر",
+    image: "assets/images/Puzzle/space/level_6.png",
+    gridSize: 7,
+    requiredStars: 20,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_7",
+    levelNumber: 7,
+    title: "النجوم البعيدة",
+    image: "assets/images/Puzzle/space/level_7.png",
+    gridSize: 7,
+    requiredStars: 30,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_8",
+    levelNumber: 8,
+    title: "أسرار الكون",
+    image: "assets/images/Puzzle/space/level_8.png",
+    gridSize: 8,
+    requiredStars: 40,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_9",
+    levelNumber: 9,
+    title: "مغامرة الفضاء",
+    image: "assets/images/Puzzle/space/level_9.png",
+    gridSize: 8,
+    requiredStars: 50,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_10",
+    levelNumber: 10,
+    title: "نهاية الرحلة",
+    image: "assets/images/Puzzle/space/level_10.png",
+    gridSize: 9,
+    requiredStars: 70,
+  ),
+
+],
+
+
+
+//==================================================
+// جزيرة الطبيعة
+//==================================================
+
+"nature": [
+
+  PuzzleLevelModel(
+    id: "level_1",
+    levelNumber: 1,
+    title: "البداية",
+    image: "assets/images/Puzzle/nature/level_1.png",
+    gridSize: 3,
+    requiredStars: 0,
+    unlocked: true,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_2",
+    levelNumber: 2,
+    title: "الجبال والأنهار",
+    image: "assets/images/Puzzle/nature/level_2.png",
+    gridSize: 4,
+    requiredStars: 3,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_3",
+    levelNumber: 3,
+    title: "جمال الطبيعة",
+    image: "assets/images/Puzzle/nature/level_3.png",
+    gridSize: 5,
+    requiredStars: 6,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_4",
+    levelNumber: 4,
+    title: "تحدي الطبيعة",
+    image: "assets/images/Puzzle/nature/level_4.png",
+    gridSize: 6,
+    requiredStars: 9,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_5",
+    levelNumber: 5,
+    title: "الغابات الخضراء",
+    image: "assets/images/Puzzle/nature/level_5.png",
+    gridSize: 6,
+    requiredStars: 15,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_6",
+    levelNumber: 6,
+    title: "الشلالات",
+    image: "assets/images/Puzzle/nature/level_6.png",
+    gridSize: 7,
+    requiredStars: 20,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_7",
+    levelNumber: 7,
+    title: "عالم النباتات",
+    image: "assets/images/Puzzle/nature/level_7.png",
+    gridSize: 7,
+    requiredStars: 30,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_8",
+    levelNumber: 8,
+    title: "روعة الطبيعة",
+    image: "assets/images/Puzzle/nature/level_8.png",
+    gridSize: 8,
+    requiredStars: 40,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_9",
+    levelNumber: 9,
+    title: "سر الطبيعة",
+    image: "assets/images/Puzzle/nature/level_9.png",
+    gridSize: 8,
+    requiredStars: 50,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_10",
+    levelNumber: 10,
+    title: "التحدي النهائي",
+    image: "assets/images/Puzzle/nature/level_10.png",
+    gridSize: 9,
+    requiredStars: 70,
+  ),
+
+],
+
+//==================================================
+// جزيرة المعالم العالمية
+//==================================================
+
+"landmarks": [
+
+  PuzzleLevelModel(
+    id: "level_1",
+    levelNumber: 1,
+    title: "البداية",
+    image: "assets/images/Puzzle/landmarks/level_1.png",
+    gridSize: 3,
+    requiredStars: 0,
+    unlocked: true,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_2",
+    levelNumber: 2,
+    title: "معالم مشهورة",
+    image: "assets/images/Puzzle/landmarks/level_2.png",
+    gridSize: 4,
+    requiredStars: 3,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_3",
+    levelNumber: 3,
+    title: "حول العالم",
+    image: "assets/images/Puzzle/landmarks/level_3.png",
+    gridSize: 5,
+    requiredStars: 6,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_4",
+    levelNumber: 4,
+    title: "تحدي المعالم",
+    image: "assets/images/Puzzle/landmarks/level_4.png",
+    gridSize: 6,
+    requiredStars: 9,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_5",
+    levelNumber: 5,
+    title: "عجائب العالم",
+    image: "assets/images/Puzzle/landmarks/level_5.png",
+    gridSize: 6,
+    requiredStars: 15,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_6",
+    levelNumber: 6,
+    title: "مدن عالمية",
+    image: "assets/images/Puzzle/landmarks/level_6.png",
+    gridSize: 7,
+    requiredStars: 20,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_7",
+    levelNumber: 7,
+    title: "آثار قديمة",
+    image: "assets/images/Puzzle/landmarks/level_7.png",
+    gridSize: 7,
+    requiredStars: 30,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_8",
+    levelNumber: 8,
+    title: "رحلة حول العالم",
+    image: "assets/images/Puzzle/landmarks/level_8.png",
+    gridSize: 8,
+    requiredStars: 40,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_9",
+    levelNumber: 9,
+    title: "أشهر المعالم",
+    image: "assets/images/Puzzle/landmarks/level_9.png",
+    gridSize: 8,
+    requiredStars: 50,
+  ),
+
+  PuzzleLevelModel(
+    id: "level_10",
+    levelNumber: 10,
+    title: "النهائي العالمي",
+    image: "assets/images/Puzzle/landmarks/level_10.png",
+    gridSize: 9,
+    requiredStars: 70,
+  ),
+
+],
 
 
   };
 
 
 
-
-
   //==================================================
-  // جلب مستويات عالم معين
+  // جلب مستويات جزيرة
   //==================================================
 
   static List<PuzzleLevelModel> getLevels(
@@ -589,10 +525,8 @@ class PuzzleLevelData {
 
 
 
-
-
   //==================================================
-  // جلب مرحلة محددة
+  // جلب مستوى محدد
   //==================================================
 
   static PuzzleLevelModel? getLevel({
@@ -607,30 +541,19 @@ class PuzzleLevelData {
     final worldLevels = getLevels(puzzleId);
 
 
-
     try {
 
-
       return worldLevels.firstWhere(
-
             (level) => level.id == levelId,
-
       );
-
 
     } catch (_) {
 
-
       return null;
-
 
     }
 
-
   }
-
-
-
 
 
 }
