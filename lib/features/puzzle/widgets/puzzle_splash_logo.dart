8329f18f7970e01c2449ffa3c9da93abbe9e3172
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-28 16:06:03 +0200

    Update puzzle_splash_logo.dart

--- lib/features/puzzle/widgets/puzzle_splash_logo.dart
+++ lib/features/puzzle/widgets/puzzle_splash_logo.dart
@@ -1,94 +1,109 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
 
+
 class PuzzleSplashLogo extends StatefulWidget {
+
   const PuzzleSplashLogo({
     super.key,
     required this.onFinished,
   });
 
+
   final VoidCallback onFinished;
+
 
   @override
   State<PuzzleSplashLogo> createState() =>
       _PuzzleSplashLogoState();
+
 }
+
 
 
 class _PuzzleSplashLogoState
     extends State<PuzzleSplashLogo>
-    with TickerProviderStateMixin {
+    with SingleTickerProviderStateMixin {
 
 
   late AnimationController controller;
 
-  bool explode = false;
 
+  final List<Offset> piecesMove = [
 
-  final List<Offset> directions = [
-
-    const Offset(-120, -80),
-    const Offset(120, -70),
-    const Offset(-100, 90),
-    const Offset(110, 100),
-    const Offset(0, -140),
+    const Offset(-140, -90),
+    const Offset(140, -80),
+    const Offset(-130, 100),
+    const Offset(130, 100),
+    const Offset(0, -160),
 
   ];
 
 
+
   @override
   void initState() {
+
     super.initState();
 
 
     controller = AnimationController(
+
       vsync: this,
+
       duration: const Duration(
-        milliseconds: 3000,
+        milliseconds: 1800,
       ),
+
     );
 
 
     startAnimation();
+
   }
+
 
 
 
   Future<void> startAnimation() async {
 
 
-    // ظهور الشعار
     await Future.delayed(
+
       const Duration(
         milliseconds: 800,
       ),
+
     );
 
 
     // انفجار القطع
-    setState(() {
-      explode = true;
-    });
-
 
     await controller.forward();
 
 
-    // انتظار رجوع القطع
+
     await Future.delayed(
+
       const Duration(
-        milliseconds: 500,
+        milliseconds: 600,
       ),
+
     );
 
 
-    controller.reverse();
+    // تجميع القطع
+
+    await controller.reverse();
+
 
 
     await Future.delayed(
+
       const Duration(
-        milliseconds: 1200,
+        milliseconds: 700,
       ),
+
     );
 
 
@@ -98,81 +113,409 @@ class _PuzzleSplashLogoState
 
 
 
+
+
+
   @override
   Widget build(BuildContext context) {
 
 
     return AnimatedBuilder(
+
       animation: controller,
+
 
       builder: (context, child) {
 
 
         return SizedBox(
-          width: 330,
+
+          width: 340,
+
           height: 180,
 
+
           child: Stack(
+
             alignment: Alignment.center,
 
+
             children: List.generate(
+
               5,
+
               (index) {
 
                 return buildPiece(index);
 
               },
+
             ),
+
           ),
+
         );
+
       },
+
     );
+
   }
 
 
-Widget buildPiece(int index) {
-
-  final Offset move =
-      explode
-          ? directions[index] *
-              controller.value
-          : Offset.zero;
 
 
-  final double rotate =
-      explode
-          ? (index.isEven ? 1 : -1) *
-              math.pi *
-              controller.value
-          : 0;
 
 
-  return Transform.translate(
 
-    offset: move,
+  Widget buildPiece(int index) {
 
-    child: Transform.rotate(
 
-      angle: rotate,
+    final movement = Offset.lerp(
 
-      child: ClipPath(
+      Offset.zero,
 
-        clipper: PuzzlePieceClipper(
-          index,
+      piecesMove[index],
+
+      controller.value,
+
+    )!;
+
+
+
+    final rotation =
+
+        (index.isEven ? 1 : -1) *
+
+        math.pi *
+
+        controller.value;
+
+
+
+    return Transform.translate(
+
+      offset: movement,
+
+
+      child: Transform.rotate(
+
+        angle: rotation,
+
+
+        child: ClipPath(
+
+          clipper: PuzzleClipper(index),
+
+
+          child: Image.asset(
+
+            "assets/images/ui/puzzle_world_splash.png",
+
+            width: 340,
+
+            height: 180,
+
+            fit: BoxFit.contain,
+
+          ),
+
         ),
 
-        child: Image.asset(
-
-          "assets/images/ui/puzzle_world_splash.png",
-
-          width: 330,
-
-          height: 180,
-
-          fit: BoxFit.contain,
-
-        ),
       ),
-    ),
-  );
+
+    );
+
+  }
+
+
+
+
+
+
+  @override
+  void dispose() {
+
+    controller.dispose();
+
+    super.dispose();
+
+  }
+
+}
+
+
+
+
+
+
+
+class PuzzleClipper extends CustomClipper<Path> {
+
+
+  final int index;
+
+
+  PuzzleClipper(this.index);
+
+
+
+  @override
+  Path getClip(Size size) {
+
+
+    final path = Path();
+
+
+    final halfW = size.width / 2;
+
+    final halfH = size.height / 2;
+
+    const tab = 18.0;
+
+
+
+    switch(index) {
+
+
+
+      case 0:
+
+        // أعلى يسار
+
+        path.moveTo(0,0);
+
+
+        path.lineTo(
+          halfW - tab,
+          0,
+        );
+
+
+        path.quadraticBezierTo(
+          halfW,
+          0,
+          halfW,
+          tab,
+        );
+
+
+        path.lineTo(
+          halfW,
+          halfH - tab,
+        );
+
+
+        path.quadraticBezierTo(
+          halfW,
+          halfH,
+          halfW - tab,
+          halfH,
+        );
+
+
+        path.lineTo(
+          0,
+          halfH,
+        );
+
+
+        path.close();
+
+        break;
+
+
+
+
+
+      case 1:
+
+        // أعلى يمين
+
+        path.moveTo(
+          halfW,
+          0,
+        );
+
+
+        path.lineTo(
+          size.width,
+          0,
+        );
+
+
+        path.lineTo(
+          size.width,
+          halfH,
+        );
+
+
+        path.lineTo(
+          halfW + tab,
+          halfH,
+        );
+
+
+        path.quadraticBezierTo(
+          halfW,
+          halfH,
+          halfW,
+          halfH - tab,
+        );
+
+
+        path.lineTo(
+          halfW,
+          tab,
+        );
+
+
+        path.quadraticBezierTo(
+          halfW,
+          0,
+          halfW + tab,
+          0,
+        );
+
+
+        path.close();
+
+        break;
+
+
+
+
+
+      case 2:
+
+        // أسفل يسار
+
+        path.moveTo(
+          0,
+          halfH,
+        );
+
+
+        path.lineTo(
+          halfW - tab,
+          halfH,
+        );
+
+
+        path.quadraticBezierTo(
+          halfW,
+          halfH,
+          halfW,
+          halfH + tab,
+        );
+
+
+        path.lineTo(
+          halfW,
+          size.height,
+        );
+
+
+        path.lineTo(
+          0,
+          size.height,
+        );
+
+
+        path.close();
+
+        break;
+
+
+
+
+
+      case 3:
+
+        // أسفل يمين
+
+        path.moveTo(
+          halfW,
+          halfH,
+        );
+
+
+        path.quadraticBezierTo(
+          halfW,
+          halfH + tab,
+          halfW + tab,
+          halfH,
+        );
+
+
+        path.lineTo(
+          size.width,
+          halfH,
+        );
+
+
+        path.lineTo(
+          size.width,
+          size.height,
+        );
+
+
+        path.lineTo(
+          halfW,
+          size.height,
+        );
+
+
+        path.close();
+
+        break;
+
+
+
+
+
+      case 4:
+
+        // القطعة الوسطى
+
+        path.addOval(
+
+          Rect.fromCenter(
+
+            center: Offset(
+              size.width / 2,
+              size.height / 2,
+            ),
+
+
+            width: 100,
+
+            height: 80,
+
+          ),
+
+        );
+
+        break;
+
+    }
+
+
+    return path;
+
+  }
+
+
+
+
+
+  @override
+  bool shouldReclip(
+      covariant CustomClipper<Path> oldClipper,
+  ) {
+
+    return false;
+
+  }
+
 }
