5783bd448a9a66d7a369f0aa018ceba6ae1dd0bf
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-28 04:36:06 +0200

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,1218 +1,320 @@
 import 'dart:async';
+import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 
 import 'features/puzzle/screens/world_map_screen.dart';
 
-
-
-
-
 class SplashScreen extends StatefulWidget {
-
-
   const SplashScreen({
-
     super.key,
-
   });
-
-
-
-
 
   @override
   State<SplashScreen> createState() =>
-
       _SplashScreenState();
-
-
 }
 
-
-
-
-
-
-
-
-
 class _SplashScreenState
-
     extends State<SplashScreen>
-
     with TickerProviderStateMixin {
 
-
-
   late AnimationController logoController;
-
-
   late AnimationController fadeController;
-
-
   late AnimationController floatController;
+  late AnimationController puzzleController;
 
-
-  late AnimationController puzzleTextController;
-
-
-
-
-
-  late Animation<double> scaleAnimation;
-
-
+  late Animation<double> logoScale;
+  late Animation<double> logoFloat;
   late Animation<double> fadeAnimation;
 
+  bool explode = false;
 
-  late Animation<double> floatAnimation;
+  final String title = "Puzzle World";
 
-
-  late Animation<double> puzzleTextAnimation;
-
-
-
-
-
-  bool showPuzzleEffect = false;
-
-
-  final List<String> puzzleLetters = [
-
-    "P",
-    "u",
-    "z",
-    "z",
-    "l",
-    "e",
-    "W",
-    "o",
-    "r",
-    "l",
-    "d",
-
+  final List<Offset> pieceOffsets = [
+    const Offset(-80,-40),
+    const Offset(-55,-90),
+    const Offset(-15,-55),
+    const Offset(25,-95),
+    const Offset(70,-50),
+    const Offset(95,-5),
+    const Offset(70,55),
+    const Offset(30,90),
+    const Offset(-25,80),
+    const Offset(-70,45),
+    const Offset(-100,0),
+    const Offset(105,35),
   ];
-
-
-
-
-  final List<Offset> puzzleOffsets = [
-
-    const Offset(-60,-40),
-
-    const Offset(20,-70),
-
-    const Offset(-40,60),
-
-    const Offset(70,30),
-
-    const Offset(-30,-60),
-
-    const Offset(50,70),
-
-    const Offset(-70,20),
-
-    const Offset(60,-50),
-
-    const Offset(-40,50),
-
-    const Offset(30,70),
-
-    const Offset(80,-20),
-
-  ];
-
-
-
-
-
-
 
   @override
-  void initState(){
-
+  void initState() {
     super.initState();
 
-
-
-
     logoController = AnimationController(
-
-      vsync:this,
-
-      duration:
-
-      const Duration(seconds:2),
-
+      vsync: this,
+      duration: const Duration(
+        seconds: 2,
+      ),
     );
-
-
-
-
 
     fadeController = AnimationController(
-
-      vsync:this,
-
-      duration:
-
-      const Duration(milliseconds:1200),
-
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 1200,
+      ),
     );
-
-
-
-
 
     floatController = AnimationController(
-
-      vsync:this,
-
-      duration:
-
-      const Duration(seconds:3),
-
-    );
-
-
-
-
-
-    puzzleTextController = AnimationController(
-
-      vsync:this,
-
-      duration:
-
-      const Duration(milliseconds:1200),
-
-    );
-
-
-
-
-
-
-
-    scaleAnimation = Tween<double>(
-
-      begin:0.85,
-
-      end:1.08,
-
-    ).animate(
-
-      CurvedAnimation(
-
-        parent:logoController,
-
-        curve:Curves.elasticOut,
-
+      vsync: this,
+      duration: const Duration(
+        seconds: 3,
       ),
-
     );
 
+    puzzleController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 1400,
+      ),
+    );
 
+    logoScale = Tween<double>(
+      begin: 0.85,
+      end: 1.08,
+    ).animate(
+      CurvedAnimation(
+        parent: logoController,
+        curve: Curves.elasticOut,
+      ),
+    );
 
-
-
-
-
+    logoFloat = Tween<double>(
+      begin: -10,
+      end: 10,
+    ).animate(
+      CurvedAnimation(
+        parent: floatController,
+        curve: Curves.easeInOut,
+      ),
+    );
 
     fadeAnimation = Tween<double>(
-
-      begin:0,
-
-      end:1,
-
+      begin: 0,
+      end: 1,
     ).animate(
-
       CurvedAnimation(
-
-        parent:fadeController,
-
-        curve:Curves.easeIn,
-
+        parent: fadeController,
+        curve: Curves.easeIn,
       ),
-
     );
-
-
-
-
-
-
-
-
-    floatAnimation = Tween<double>(
-
-      begin:-10,
-
-      end:10,
-
-    ).animate(
-
-      CurvedAnimation(
-
-        parent:floatController,
-
-        curve:Curves.easeInOut,
-
-      ),
-
-    );
-
-
-
-
-
-
-
-    puzzleTextAnimation = Tween<double>(
-
-      begin:0,
-
-      end:1,
-
-    ).animate(
-
-      CurvedAnimation(
-
-        parent:puzzleTextController,
-
-        curve:Curves.easeOutBack,
-
-      ),
-
-    );
-
-
-
-
-
-
 
     fadeController.forward();
 
-
-
-
-
     logoController.repeat(
-
-      reverse:true,
-
+      reverse: true,
     );
-
-
-
-
 
     floatController.repeat(
-
-      reverse:true,
-
+      reverse: true,
     );
 
-
-
-
-
-    startSplashTimer();
-
-
+    startAnimation();
   }
 
-
-
-
-
-
-
-  Future<void> startSplashTimer() async {
-
-
-
+  Future<void> startAnimation() async {
     await Future.delayed(
-
-      const Duration(seconds:3),
-
+      const Duration(
+        seconds: 3,
+      ),
     );
 
+    if (!mounted) return;
 
-
-
-    if(!mounted){
-
-      return;
-
-    }
-
-
-
-
-    setState((){
-
-
-      showPuzzleEffect = true;
-
-
+    setState(() {
+      explode = true;
     });
 
-
-
-
-
-    await puzzleTextController.forward();
-
-
-
-
+    await puzzleController.forward();
 
     await Future.delayed(
-
-      const Duration(milliseconds:800),
-
+      const Duration(
+        milliseconds: 500,
+      ),
     );
 
-
-
-
-
-    if(!mounted){
-
-      return;
-
-    }
-
-
-
-
+    if (!mounted) return;
 
     Navigator.pushReplacement(
-
       context,
-
       MaterialPageRoute(
-
-        builder:(_)=>
-
-        const WorldMapScreen(),
-
+        builder: (_) =>
+            const WorldMapScreen(),
       ),
-
     );
-
-
   }
 
-
-  Widget buildLogo(){
-
-
+  Widget buildLogo() {
     return AnimatedBuilder(
-
-      animation:
-
-      Listenable.merge([
-
+      animation: Listenable.merge([
         logoController,
-
         floatController,
-
       ]),
-
-
-
-      builder:(context,child){
-
-
+      builder: (context, child) {
         return Transform.translate(
-
-
-          offset:
-
-          Offset(
-
+          offset: Offset(
             0,
-
-            floatAnimation.value,
-
+            logoFloat.value,
           ),
-
-
-
-          child:
-
-          Transform.scale(
-
-
-            scale:
-
-            scaleAnimation.value,
-
-
-
-            child:child,
-
-
+          child: Transform.scale(
+            scale: logoScale.value,
+            child: child,
           ),
-
-
-
         );
-
-
       },
-
-
-
-      child:
-
-      Image.asset(
-
+      child: Image.asset(
         "assets/images/ui/puzzle_logo.png",
-
-
-        width:220,
-
-
-        height:220,
-
-
-        fit:
-
-        BoxFit.contain,
-
-
+        width: 220,
+        height: 220,
+        fit: BoxFit.contain,
       ),
-
-
     );
-
-
   }
 
-
-
-
-
-
-
-
-
-  Widget buildPuzzleTitle(){
-
-
+  Widget buildPuzzleTitle() {
     return AnimatedBuilder(
+      animation: puzzleController,
+      builder: (context, child) {
+        return Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 0,
+          children: List.generate(
+            title.length,
+            (index) {
+              final letter = title[index];
 
+              if (letter == " ") {
+                return const SizedBox(width: 14);
+              }
 
-      animation:
+              final Offset target =
+                  pieceOffsets[index % pieceOffsets.length];
 
-      puzzleTextAnimation,
-
-
-
-      builder:(context,child){
-
-
-
-        return Row(
-
-
-          mainAxisAlignment:
-
-          MainAxisAlignment.center,
-
-
-
-          children:
-
-          List.generate(
-
-            puzzleLetters.length,
-
-
-            (index){
-
-
-
-              final offset =
-
-              showPuzzleEffect
-
-                  ? puzzleOffsets[index] *
-
-                  puzzleTextAnimation.value
-
+              final Offset movement = explode
+                  ? Offset(
+                      target.dx * puzzleController.value,
+                      target.dy * puzzleController.value,
+                    )
                   : Offset.zero;
 
-
-
-
-
+              final double rotation = explode
+                  ? (index.isEven ? 1 : -1) *
+                      math.pi *
+                      puzzleController.value
+                  : 0;
 
               return Transform.translate(
-
-
-
-                offset:offset,
-
-
-
-                child:
-
-                Transform.rotate(
-
-
-
-                  angle:
-
-                  showPuzzleEffect
-
-                      ? (index.isEven
-
-                      ? 0.25
-
-                      : -0.25)
-
-                      *
-
-                      puzzleTextAnimation.value
-
-                      :0,
-
-
-
-                  child:
-
-                  AnimatedOpacity(
-
-
-
-                    opacity:
-
-                    1 -
-
-                    puzzleTextAnimation.value * 0.2,
-
-
-
-                    duration:
-
-                    const Duration(
-
-                      milliseconds:300,
-
-                    ),
-
-
-
-                    child:
-
-                    Text(
-
-
-
-                      puzzleLetters[index],
-
-
-
-                      style:
-
-                      const TextStyle(
-
-
-
-                        fontFamily:
-
-                        "Cairo",
-
-
-
-                        fontSize:
-
-                        42,
-
-
-
-                        fontWeight:
-
-                        FontWeight.w900,
-
-
-
-                        color:
-
-                        Color(0xff2196F3),
-
-
-
-                        shadows:[
-
-
-
-                          Shadow(
-
-                            color:
-
-                            Colors.black,
-
-                            blurRadius:
-
-                            0,
-
-                            offset:
-
-                            Offset(2,2),
-
-                          ),
-
-
-
-                          Shadow(
-
-                            color:
-
-                            Colors.black87,
-
-                            blurRadius:
-
-                            8,
-
-                            offset:
-
-                            Offset(0,4),
-
-                          ),
-
-
-
-                        ],
-
-
-
+                offset: movement,
+                child: Transform.rotate(
+                  angle: rotation,
+                  child: Opacity(
+                    opacity: 1 - (0.25 * puzzleController.value),
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 1,
                       ),
-
-
-
-                    ),
-
-
-                  ),
-
-
-                ),
-
-
-              );
-
-
-
-            },
-
-          ),
-
-
-        );
-
-
-      },
-
-
-    );
-
-
-  }
-
-  Widget buildLogo(){
-
-
-    return AnimatedBuilder(
-
-      animation:
-
-      Listenable.merge([
-
-        logoController,
-
-        floatController,
-
-      ]),
-
-
-
-      builder:(context,child){
-
-
-        return Transform.translate(
-
-
-          offset:
-
-          Offset(
-
-            0,
-
-            floatAnimation.value,
-
-          ),
-
-
-
-          child:
-
-          Transform.scale(
-
-
-            scale:
-
-            scaleAnimation.value,
-
-
-
-            child:child,
-
-
-          ),
-
-
-
-        );
-
-
-      },
-
-
-
-      child:
-
-      Image.asset(
-
-        "assets/images/ui/puzzle_logo.png",
-
-
-        width:220,
-
-
-        height:220,
-
-
-        fit:
-
-        BoxFit.contain,
-
-
-      ),
-
-
-    );
-
-
-  }
-
-
-
-
-
-
-
-
-
-  Widget buildPuzzleTitle(){
-
-
-    return AnimatedBuilder(
-
-
-      animation:
-
-      puzzleTextAnimation,
-
-
-
-      builder:(context,child){
-
-
-
-        return Row(
-
-
-          mainAxisAlignment:
-
-          MainAxisAlignment.center,
-
-
-
-          children:
-
-          List.generate(
-
-            puzzleLetters.length,
-
-
-            (index){
-
-
-
-              final offset =
-
-              showPuzzleEffect
-
-                  ? puzzleOffsets[index] *
-
-                  puzzleTextAnimation.value
-
-                  : Offset.zero;
-
-
-
-
-
-
-              return Transform.translate(
-
-
-
-                offset:offset,
-
-
-
-                child:
-
-                Transform.rotate(
-
-
-
-                  angle:
-
-                  showPuzzleEffect
-
-                      ? (index.isEven
-
-                      ? 0.25
-
-                      : -0.25)
-
-                      *
-
-                      puzzleTextAnimation.value
-
-                      :0,
-
-
-
-                  child:
-
-                  AnimatedOpacity(
-
-
-
-                    opacity:
-
-                    1 -
-
-                    puzzleTextAnimation.value * 0.2,
-
-
-
-                    duration:
-
-                    const Duration(
-
-                      milliseconds:300,
-
-                    ),
-
-
-
-                    child:
-
-                    Text(
-
-
-
-                      puzzleLetters[index],
-
-
-
-                      style:
-
-                      const TextStyle(
-
-
-
-                        fontFamily:
-
-                        "Cairo",
-
-
-
-                        fontSize:
-
-                        42,
-
-
-
-                        fontWeight:
-
-                        FontWeight.w900,
-
-
-
-                        color:
-
-                        Color(0xff2196F3),
-
-
-
-                        shadows:[
-
-
-
-                          Shadow(
-
-                            color:
-
-                            Colors.black,
-
-                            blurRadius:
-
-                            0,
-
-                            offset:
-
-                            Offset(2,2),
-
-                          ),
-
-
-
-                          Shadow(
-
-                            color:
-
-                            Colors.black87,
-
-                            blurRadius:
-
-                            8,
-
-                            offset:
-
-                            Offset(0,4),
-
-                          ),
-
-
-
-                        ],
-
-
-
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 2,
+                        vertical: 2,
                       ),
-
-
-
+                      decoration: BoxDecoration(
+                        color: const Color(0xff2196F3),
+                        borderRadius:
+                            BorderRadius.circular(6),
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 1.5,
+                        ),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 6,
+                            offset: Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        letter,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 38,
+                          fontWeight: FontWeight.w900,
+                          fontFamily: "Cairo",
+                        ),
+                      ),
                     ),
-
-
                   ),
-
-
                 ),
-
-
               );
-
-
-
             },
-
           ),
-
-
         );
-
-
       },
-
-
     );
-
-
   }
 
   @override
-  Widget build(BuildContext context){
-
-
+  Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
 
-
-      body:
-
-      Stack(
-
-
-        fit:
-
-        StackFit.expand,
-
-
-
-        children:[
-
-
-
-
-
+          // الخلفية
           Image.asset(
-
             "assets/images/background/home_background.png",
-
-            fit:
-
-            BoxFit.cover,
-
+            fit: BoxFit.cover,
           ),
 
-
-
-
-
-
+          // طبقة تعتيم خفيفة
           Container(
-
-            color:
-
-            Colors.black.withOpacity(.18),
-
+            color: Colors.black.withOpacity(0.18),
           ),
 
-
-
-
-
-
-
+          // الشعار
           Positioned(
-
-            top:
-
-            MediaQuery.of(context).size.height * .22,
-
-
-            left:0,
-
-
-            right:0,
-
-
-
-            child:
-
-            FadeTransition(
-
-              opacity:
-
-              fadeAnimation,
-
-
-
-              child:
-
-              Center(
-
-                child:
-
-                buildLogo(),
-
+            top: MediaQuery.of(context).size.height * 0.20,
+            left: 0,
+            right: 0,
+            child: FadeTransition(
+              opacity: fadeAnimation,
+              child: Center(
+                child: buildLogo(),
               ),
-
             ),
-
           ),
 
-
-
-
-
-
-
+          // عنوان اللعبة
           Positioned(
-
-            top:
-
-            MediaQuery.of(context).size.height * .55,
-
-
-            left:0,
-
-
-            right:0,
-
-
-
-            child:
-
-            Center(
-
-              child:
-
-              buildPuzzleTitle(),
-
+            top: MediaQuery.of(context).size.height * 0.56,
+            left: 20,
+            right: 20,
+            child: Center(
+              child: buildPuzzleTitle(),
             ),
-
           ),
-
-
-
-
-
         ],
-
       ),
-
-
     );
-
-
   }
 
-
-
-
-
-
-
-
-
   @override
-  void dispose(){
-
-
-
+  void dispose() {
     logoController.dispose();
-
-
     fadeController.dispose();
-
-
     floatController.dispose();
-
-
-    puzzleTextController.dispose();
-
-
+    puzzleController.dispose();
 
     super.dispose();
-
-
-
   }
-
-
-
 }
