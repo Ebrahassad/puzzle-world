36eaad71e4b6f144be1768208a3d741d40fd13af
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-29 19:43:34 +0200

    Create island_background_data.dart

---
+++ lib/features/puzzle/data/island_background_data.dart
@@ -0,0 +1,84 @@
+class IslandBackgroundData {
+
+
+  static const Map<String, String> backgrounds = {
+
+
+
+    //==========================================
+    // جزيرة الحيوانات
+    //==========================================
+
+    "animals":
+
+    "assets/images/background/animals_background.png",
+
+
+
+
+
+    //==========================================
+    // جزيرة السيارات
+    //==========================================
+
+    "cars":
+
+    "assets/images/background/cars_background.png",
+
+
+
+
+
+    //==========================================
+    // جزيرة الطبيعة
+    //==========================================
+
+    "nature":
+
+    "assets/images/background/nature_background.png",
+
+
+
+
+
+    //==========================================
+    // جزيرة المعالم العالمية
+    //==========================================
+
+    "landmarks":
+
+    "assets/images/background/landmarks_background.png",
+
+
+
+
+
+    //==========================================
+    // جزيرة الفضاء
+    //==========================================
+
+    "space":
+
+    "assets/images/background/space_background.png",
+
+
+  };
+
+
+
+
+
+
+  static String getBackground(String islandId) {
+
+
+    return backgrounds[islandId] ??
+
+        "assets/images/background/splash_background.png";
+
+
+  }
+
+
+
+}
