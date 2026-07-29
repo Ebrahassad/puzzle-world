c98e66bdba80095a06a5a0ec091a3cedb776292b
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-29 17:29:19 +0200

    Update island_map_data.dart

--- lib/features/puzzle/data/island_map_data.dart
+++ lib/features/puzzle/data/island_map_data.dart
@@ -2,34 +2,80 @@ import 'package:flutter/material.dart';
 
 import '../models/island_map_model.dart';
 
-
-
 class IslandMapData {
 
+  static const Map<String, IslandMapModel> positions = {
 
-  static const Map<String, IslandMapModel>
-      positions = {
+    //==========================================
+    // SPACE
+    //==========================================
 
-
-    // هنا نضع الجزر بعد ضبط الصورة
-
-
-    "island_1":
-
-    IslandMapModel(
+    "island_1": IslandMapModel(
 
       islandId: "island_1",
 
-      position:
-      Offset(100, 250),
+      position: Offset(572, 205),
 
-      size:
-      140,
+      size: 170,
 
     ),
 
+    //==========================================
+    // WORLD LANDMARKS
+    //==========================================
+
+    "island_2": IslandMapModel(
+
+      islandId: "island_2",
+
+      position: Offset(856, 629),
+
+      size: 170,
+
+    ),
+
+    //==========================================
+    // ANIMALS
+    //==========================================
+
+    "island_3": IslandMapModel(
+
+      islandId: "island_3",
+
+      position: Offset(356, 593),
+
+      size: 170,
+
+    ),
+
+    //==========================================
+    // NATURE
+    //==========================================
+
+    "island_4": IslandMapModel(
+
+      islandId: "island_4",
+
+      position: Offset(844, 1099),
+
+      size: 170,
+
+    ),
+
+    //==========================================
+    // CARS
+    //==========================================
+
+    "island_5": IslandMapModel(
+
+      islandId: "island_5",
+
+      position: Offset(376, 1099),
+
+      size: 170,
+
+    ),
 
   };
-
 
 }
