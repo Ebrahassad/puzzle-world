55aed5bb7b5ef793281e601671241b65fd9ce27c
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-30 18:06:59 +0200

    Update puzzle_piece_widget.dart

--- lib/features/puzzle/widgets/puzzle_piece_widget.dart
+++ lib/features/puzzle/widgets/puzzle_piece_widget.dart
@@ -5,136 +5,240 @@ import 'package:flutter/material.dart';
 import '../engine/puzzle_piece.dart';
 import '../engine/puzzle_painter.dart';
 
+
+
 class PuzzlePieceWidget extends StatefulWidget {
 
+
   final PuzzlePiece piece;
+
   final ImageProvider image;
+
   final double size;
 
+
+
   const PuzzlePieceWidget({
+
     super.key,
+
     required this.piece,
+
     required this.image,
+
     required this.size,
+
   });
+
+
 
   @override
   State<PuzzlePieceWidget> createState() =>
       _PuzzlePieceWidgetState();
+
 }
+
+
+
+
 
 class _PuzzlePieceWidgetState
     extends State<PuzzlePieceWidget> {
 
+
   ui.Image? cachedImage;
 
-  ImageStream? _stream;
 
-  ImageStreamListener? _listener;
+  ImageStream? imageStream;
+
+
+  ImageStreamListener? listener;
+
+
+
 
   @override
   void didChangeDependencies() {
+
     super.didChangeDependencies();
+
     _loadImage();
+
   }
+
+
+
+
 
   @override
   void didUpdateWidget(
+
     covariant PuzzlePieceWidget oldWidget,
+
   ) {
+
+
     super.didUpdateWidget(oldWidget);
 
+
+
     if (oldWidget.image != widget.image) {
+
       _loadImage();
+
     }
+
   }
+
+
+
+
 
   void _loadImage() {
 
-  if (_listener != null && _stream != null) {
-    _stream!.removeListener(_listener!);
-  }
 
-  final stream = widget.image.resolve(
-    createLocalImageConfiguration(context),
-  );
+    if (listener != null && imageStream != null) {
 
-  _stream = stream;
-
-  _listener = ImageStreamListener(
-
-    (info, _) {
-
-      if (!mounted) return;
-
-      setState(() {
-        cachedImage = info.image;
-      });
-
-    },
-
-    onError: (error, stackTrace) {
-
-      debugPrint(
-        "Puzzle image error : $error",
-      );
-
-    },
-
-  );
-
-  stream.addListener(_listener!);
-
-}
-
-  @override
-  Widget build(BuildContext context) {
-
-    if (cachedImage == null) {
-
-      return SizedBox(
-        width: widget.size,
-        height: widget.size,
-      );
+      imageStream!.removeListener(listener!);
 
     }
 
-    return RepaintBoundary(
 
-      child: CustomPaint(
 
-        size: Size(
-          widget.size,
-          widget.size,
-        ),
+    imageStream = widget.image.resolve(
 
-        painter: PuzzlePainter(
+      createLocalImageConfiguration(
 
-          piece: widget.piece,
-
-          image: widget.image,
-
-          cachedImage: cachedImage,
-
-        ),
+        context,
 
       ),
 
     );
 
+
+
+    listener = ImageStreamListener(
+
+      (info, _) {
+
+
+        if (!mounted) return;
+
+
+
+        setState(() {
+
+
+          cachedImage = info.image;
+
+
+        });
+
+
+      },
+
+
+      onError: (error, stackTrace) {
+
+
+        debugPrint(
+
+          "Puzzle image error: $error",
+
+        );
+
+
+      },
+
+    );
+
+
+
+    imageStream!.addListener(listener!);
+
   }
 
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    if (cachedImage == null) {
+
+
+      return SizedBox(
+
+        width: widget.size,
+
+        height: widget.size,
+
+      );
+
+    }
+
+
+
+    return RepaintBoundary(
+
+
+      child: CustomPaint(
+
+
+        size: Size(
+
+          widget.size,
+
+          widget.size,
+
+        ),
+
+
+
+        painter: PuzzlePainter(
+
+
+          piece: widget.piece,
+
+
+          image: widget.image,
+
+
+          cachedImage: cachedImage,
+
+
+        ),
+
+
+      ),
+
+
+    );
+
+
+  }
   @override
   void dispose() {
 
-    if (_listener != null && _stream != null) {
 
-      _stream!.removeListener(_listener!);
+    if (listener != null && imageStream != null) {
+
+
+      imageStream!.removeListener(
+
+        listener!,
+
+      );
+
 
     }
+
 
     super.dispose();
 
   }
+
 
 }
