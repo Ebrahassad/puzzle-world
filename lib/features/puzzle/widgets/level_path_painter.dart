d569702a7d855897146da3c05420e18a70c49aa8
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-31 02:57:27 +0200

    Update level_path_painter.dart

--- lib/features/puzzle/widgets/level_path_painter.dart
+++ lib/features/puzzle/widgets/level_path_painter.dart
@@ -4,34 +4,67 @@ import 'package:flutter/material.dart';
 class LevelPathPainter extends CustomPainter {
 
 
+  final List<Offset> positions;
+
+
+  LevelPathPainter({
+    required this.positions,
+  });
+
+
+
   @override
   void paint(
     Canvas canvas,
     Size size,
   ) {
 
+
     final paint = Paint()
-      ..color = Colors.white.withOpacity(0.6)
+
+      ..color = Colors.white.withOpacity(0.65)
+
       ..strokeWidth = 8
+
       ..style = PaintingStyle.stroke
+
       ..strokeCap = StrokeCap.round;
+
 
 
     final path = Path();
 
 
+
+    if (positions.isEmpty) return;
+
+
+
     path.moveTo(
-      size.width * 0.25,
-      size.height * 0.05,
+      size.width * positions[0].dx + 65,
+      size.height * (0.58 + positions[0].dy) + 65,
     );
 
 
-    path.quadraticBezierTo(
-      size.width * 0.75,
-      size.height * 0.15,
-      size.width * 0.25,
-      size.height * 0.30,
-    );
+
+    for(int i = 1; i < positions.length; i++) {
+
+
+      path.quadraticBezierTo(
+
+        size.width * positions[i].dx + 65,
+
+        size.height * (0.58 + positions[i].dy) + 20,
+
+        size.width * positions[i].dx + 65,
+
+        size.height * (0.58 + positions[i].dy) + 65,
+
+      );
+
+
+    }
+
 
 
     canvas.drawPath(
@@ -42,10 +75,14 @@ class LevelPathPainter extends CustomPainter {
   }
 
 
+
   @override
   bool shouldRepaint(
     CustomPainter oldDelegate,
   ) {
-    return false;
+
+    return true;
+
   }
+
 }
