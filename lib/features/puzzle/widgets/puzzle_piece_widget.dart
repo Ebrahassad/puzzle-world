92d8c813686fe4d33456ec0251b1e413d1c2af14
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-31 04:15:59 +0200

    Update puzzle_piece_widget.dart

--- lib/features/puzzle/widgets/puzzle_piece_widget.dart
+++ lib/features/puzzle/widgets/puzzle_piece_widget.dart
@@ -5,119 +5,402 @@ import 'package:flutter/material.dart';
 import '../engine/puzzle_piece.dart';
 import '../engine/puzzle_painter.dart';
 
+
 class PuzzlePieceWidget extends StatefulWidget {
+
   final PuzzlePiece piece;
+
   final ImageProvider image;
+
   final double size;
 
+
   final bool isActive;
+
   final double opacity;
 
+
+
   const PuzzlePieceWidget({
+
     super.key,
+
     required this.piece,
+
     required this.image,
+
     required this.size,
+
     this.isActive = false,
+
     this.opacity = 1.0,
+
   });
 
+
+
   @override
-  State<PuzzlePieceWidget> createState() => _PuzzlePieceWidgetState();
+  State<PuzzlePieceWidget> createState() =>
+      _PuzzlePieceWidgetState();
+
 }
 
-class _PuzzlePieceWidgetState extends State<PuzzlePieceWidget> {
+
+
+
+
+
+class _PuzzlePieceWidgetState
+
+    extends State<PuzzlePieceWidget> {
+
+
+
   ui.Image? cachedImage;
-  ImageStream? _imageStream;
+
+
+  ImageStream? _stream;
+
+
   ImageStreamListener? _listener;
-  bool _loaded = false;
+
+
+
+  bool loaded = false;
+
+
+
+
+
 
   @override
   void didChangeDependencies() {
+
     super.didChangeDependencies();
 
-    if (!_loaded) {
+
+    if(!loaded){
+
       _loadImage();
+
     }
+
   }
 
+
+
+
+
+
+
+
   @override
-  void didUpdateWidget(covariant PuzzlePieceWidget oldWidget) {
+  void didUpdateWidget(
+
+    covariant PuzzlePieceWidget oldWidget,
+
+  ){
+
     super.didUpdateWidget(oldWidget);
 
-    if (oldWidget.image != widget.image) {
-      _loaded = false;
+
+
+    if(oldWidget.image != widget.image){
+
+
+      _removeListener();
+
+
+
       cachedImage = null;
+
+
+      loaded = false;
+
+
       _loadImage();
+
+
     }
+
+
   }
 
-  void _loadImage() {
-    if (_listener != null && _imageStream != null) {
-      _imageStream!.removeListener(_listener!);
-    }
 
-    final configuration = createLocalImageConfiguration(
-      context,
-      size: Size(widget.size, widget.size),
+
+
+
+
+
+
+
+  void _loadImage(){
+
+
+
+    final configuration =
+
+        createLocalImageConfiguration(
+
+          context,
+
+          size: Size(
+
+            widget.size,
+
+            widget.size,
+
+          ),
+
+        );
+
+
+
+    _stream = widget.image.resolve(
+
+      configuration,
+
     );
 
-    _imageStream = widget.image.resolve(configuration);
+
+
 
     _listener = ImageStreamListener(
-      (info, _) {
-        if (!mounted) return;
 
-        setState(() {
+      (info, synchronousCall){
+
+
+        if(!mounted) return;
+
+
+
+        setState((){
+
+
           cachedImage = info.image;
-          _loaded = true;
+
+
+          loaded = true;
+
+
+
         });
+
+
+
       },
-      onError: (error, stackTrace) {
-        debugPrint("Puzzle image error: $error");
+
+
+      onError: (error, stackTrace){
+
+
+        debugPrint(
+
+          "Puzzle image loading error: $error",
+
+        );
+
+
       },
+
+
     );
 
-    _imageStream!.addListener(_listener!);
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    final piece = cachedImage == null
-        ? SizedBox(
-            width: widget.size,
-            height: widget.size,
-          )
-        : RepaintBoundary(
-            child: CustomPaint(
-              size: Size(widget.size, widget.size),
-              painter: PuzzlePainter(
-                piece: widget.piece,
-                image: widget.image,
-                cachedImage: cachedImage,
-              ),
-            ),
-          );
 
-    return AnimatedScale(
-      scale: widget.isActive ? 1.06 : 1.0,
-      duration: const Duration(milliseconds: 140),
-      curve: Curves.easeOutCubic,
-      child: AnimatedOpacity(
-        opacity: widget.opacity,
-        duration: const Duration(milliseconds: 120),
-        curve: Curves.easeOut,
-        child: piece,
-      ),
+
+    _stream!.addListener(
+
+      _listener!,
+
     );
+
+
   }
 
-  @override
-  void dispose() {
-    if (_listener != null && _imageStream != null) {
-      _imageStream!.removeListener(_listener!);
+
+
+
+
+
+
+
+
+  void _removeListener(){
+
+
+    if(_listener != null &&
+
+       _stream != null){
+
+
+
+      _stream!.removeListener(
+
+        _listener!,
+
+      );
+
     }
 
-    super.dispose();
+
   }
+
+
+
+
+
+
+
+
+
+
+  @override
+  Widget build(
+
+    BuildContext context,
+
+  ){
+
+
+
+    Widget child;
+
+
+
+    if(cachedImage == null){
+
+
+
+      child = SizedBox(
+
+        width: widget.size,
+
+        height: widget.size,
+
+      );
+
+
+
+    }
+
+    else {
+
+
+
+      child = RepaintBoundary(
+
+
+        child: CustomPaint(
+
+
+          size: Size(
+
+            widget.size,
+
+            widget.size,
+
+          ),
+
+
+
+          painter: PuzzlePainter(
+
+
+            piece: widget.piece,
+
+
+            image: widget.image,
+
+
+            cachedImage: cachedImage,
+
+
+          ),
+
+
+        ),
+
+
+      );
+
+
+    }
+
+
+
+
+
+
+    return AnimatedScale(
+
+      scale:
+
+          widget.isActive
+
+              ? 1.08
+
+              : 1.0,
+
+
+      duration:
+
+          const Duration(
+
+            milliseconds: 140,
+
+          ),
+
+
+      curve:
+
+          Curves.easeOutCubic,
+
+
+
+      child: AnimatedOpacity(
+
+
+        opacity: widget.opacity,
+
+
+        duration:
+
+            const Duration(
+
+              milliseconds: 120,
+
+            ),
+
+
+
+        child: child,
+
+      ),
+
+    );
+
+
+  }
+
+
+
+
+
+
+
+
+
+  @override
+  void dispose(){
+
+
+    _removeListener();
+
+
+    super.dispose();
+
+  }
+
+
 }
