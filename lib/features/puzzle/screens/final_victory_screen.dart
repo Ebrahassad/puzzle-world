181663f4c41aeb5bfee7700bccb31286b427ed2c
Ebrahassad <ebrahassadi1@gmail.com>
2026-08-04 22:49:19 +0200

    Update final_victory_screen.dart

--- lib/features/puzzle/screens/final_victory_screen.dart
+++ lib/features/puzzle/screens/final_victory_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import '../managers/reward_manager.dart';
 import 'world_map_screen.dart';
 
