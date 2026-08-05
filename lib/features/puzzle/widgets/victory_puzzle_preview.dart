336c5883736a5d262138dc8d6734a5ebface6dfd
Ebrahassad <ebrahassadi1@gmail.com>
2026-08-05 17:42:58 +0200

    Update victory_puzzle_preview.dart

--- lib/features/puzzle/widgets/victory_puzzle_preview.dart
+++ lib/features/puzzle/widgets/victory_puzzle_preview.dart
@@ -4,9 +4,34 @@ import 'package:flutter/material.dart';
 
 import '../engine/puzzle_piece.dart';
 
+
+/// Data used only for victory cinematic rendering.
+/// Does not modify the real puzzle engine.
+class VictoryPieceRenderData {
+  final PuzzlePiece piece;
+
+  final Offset position;
+  final double rotation;
+  final double opacity;
+  final double scale;
+
+  VictoryPieceRenderData({
+    required this.piece,
+    required this.position,
+    required this.rotation,
+    required this.opacity,
+    required this.scale,
+  });
+}
+
+
+
 class VictoryPuzzlePreview extends StatelessWidget {
+
   final ui.Image image;
-  final List<PuzzlePiece> pieces;
+
+  final List<VictoryPieceRenderData> pieces;
+
 
   const VictoryPuzzlePreview({
     super.key,
@@ -14,10 +39,14 @@ class VictoryPuzzlePreview extends StatelessWidget {
     required this.pieces,
   });
 
+
+
   @override
   Widget build(BuildContext context) {
+
     return CustomPaint(
       size: Size.infinite,
+
       painter: _VictoryPuzzlePainter(
         pieces: pieces,
         image: image,
@@ -27,75 +56,151 @@ class VictoryPuzzlePreview extends StatelessWidget {
 }
 
 
+
+
+
 class _VictoryPuzzlePainter extends CustomPainter {
 
-  final List<PuzzlePiece> pieces;
+
+  final List<VictoryPieceRenderData> pieces;
+
   final ui.Image image;
+
 
   _VictoryPuzzlePainter({
     required this.pieces,
     required this.image,
   });
 
+
+
   final Paint _paint = Paint()
     ..filterQuality = FilterQuality.high;
+
 
 
   @override
   void paint(Canvas canvas, Size size) {
 
-    for (final piece in pieces) {
+
+    for (final data in pieces) {
+
+
+      final piece = data.piece;
+
 
       canvas.save();
 
+
+
+      // Current animated position
       canvas.translate(
-        piece.correctPosition.dx,
-        piece.correctPosition.dy,
+        data.position.dx,
+        data.position.dy,
       );
 
-      canvas.clipPath(piece.path);
+
+
+      // Explosion rotation
+      canvas.rotate(
+        data.rotation,
+      );
+
+
+
+      // Explosion scale
+      canvas.scale(
+        data.scale,
+        data.scale,
+      );
+
+
+
+      // Fade animation
+      _paint.opacity = data.opacity;
+
+
+
+      canvas.clipPath(
+        piece.path,
+      );
+
 
 
       final source = Rect.fromLTWH(
+
         piece.col *
             (image.width / _getCols()),
+
         piece.row *
             (image.height / _getRows()),
+
         image.width / _getCols(),
+
         image.height / _getRows(),
+
       );
 
 
+
       canvas.drawImageRect(
+
         image,
+
         source,
+
         Rect.fromLTWH(
           0,
           0,
           piece.width,
           piece.height,
         ),
+
         _paint,
+
       );
 
 
+
       canvas.restore();
+
     }
+
   }
+
+
 
 
   int _getCols() {
-    return pieces.map((e) => e.col).reduce((a,b)=>a>b?a:b)+1;
+
+    return pieces
+            .map((e) => e.piece.col)
+            .reduce((a, b) => a > b ? a : b) +
+        1;
+
   }
 
+
+
+
   int _getRows() {
-    return pieces.map((e) => e.row).reduce((a,b)=>a>b?a:b)+1;
+
+    return pieces
+            .map((e) => e.piece.row)
+            .reduce((a, b) => a > b ? a : b) +
+        1;
+
   }
+
+
 
 
   @override
   bool shouldRepaint(
       covariant _VictoryPuzzlePainter oldDelegate) {
+
     return true;
+
   }
+
 }
