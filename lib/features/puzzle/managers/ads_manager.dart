ee706a26edc71be9ac05b281e82187e85602bdd7
Ebrahassad <ebrahassadi1@gmail.com>
2026-08-06 16:44:53 +0200

    Update ads_manager.dart

--- lib/features/puzzle/managers/ads_manager.dart
+++ lib/features/puzzle/managers/ads_manager.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:unity_ads_plugin/unity_ads_plugin.dart';
@@ -21,38 +22,69 @@ class AdsManager {
 
   bool get isInitialized => _initialized;
 
+  bool _isShowing = false;
+
+  bool get isShowing => _isShowing;
+
   Future<void> initAds() async {
+
     if (_initialized) return;
+
+    final completer = Completer<void>();
 
     UnityAds.init(
       gameId: _gameId,
       testMode: testMode,
+
       onComplete: () {
         _initialized = true;
         debugPrint('✅ Unity Ads initialized');
+        completer.complete();
       },
+
       onFailed: (error, message) {
         debugPrint('❌ Unity Ads init failed: $message');
+        completer.complete();
       },
     );
+
+    return completer.future;
   }
 
   void showRewardedAd({
     required VoidCallback onRewardEarned,
     VoidCallback? onAdFailed,
   }) {
+
     if (!_initialized) {
       onAdFailed?.call();
       return;
     }
 
+    if (_isShowing) {
+      onAdFailed?.call();
+      return;
+    }
+
+    _isShowing = true;
+
     UnityAds.showVideoAd(
       placementId: rewardedPlacementId,
+
       onComplete: (_) {
+
+        _isShowing = false;
+
         onRewardEarned();
+
       },
+
       onFailed: (_, __, ___) {
+
+        _isShowing = false;
+
         onAdFailed?.call();
+
       },
     );
   }
@@ -60,19 +92,38 @@ class AdsManager {
   void showInterstitialAd({
     required VoidCallback onAdClosed,
   }) {
+
     if (!_initialized) {
       onAdClosed();
       return;
     }
 
+    if (_isShowing) {
+      onAdClosed();
+      return;
+    }
+
+    _isShowing = true;
+
     UnityAds.showVideoAd(
       placementId: interstitialPlacementId,
+
       onComplete: (_) {
+
+        _isShowing = false;
+
         onAdClosed();
+
       },
+
       onFailed: (_, __, ___) {
+
+        _isShowing = false;
+
         onAdClosed();
+
       },
+
     );
   }
 
