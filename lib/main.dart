3ae7837de1d763a26aefc1f448185a75cf550dbb
Ebrahassad <ebrahassadi1@gmail.com>
2026-08-06 16:27:40 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'core/theme/app_theme.dart';
+import 'features/puzzle/managers/ads_manager.dart';
 import 'features/puzzle/screens/world_map_screen.dart';
 
-
-void main() async {
-
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await MobileAds.instance.initialize();
+  // تهيئة Unity Ads
+  await AdsManager().initAds();
 
   runApp(
     const PuzzleWorldApp(),
   );
-
 }
 
-
-
 class PuzzleWorldApp extends StatelessWidget {
-
   const PuzzleWorldApp({
     super.key,
   });
 
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
-      debugShowCheckedModeBanner:false,
-
-      title:"Puzzle World",
-
+      debugShowCheckedModeBanner: false,
+      title: "Puzzle World",
       theme: AppTheme.lightTheme,
-
       home: const WorldMapScreen(),
-
     );
-
   }
-
 }
