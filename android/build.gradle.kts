97700678ff757c0d6996d877bd36acdbcf56aa88
Ebrahassad <ebrahassadi1@gmail.com>
2026-08-07 14:12:04 +0200

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,17 +17,6 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 
     project.evaluationDependsOn(":app")
-
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        }
-    }
-
-    tasks.withType<JavaCompile>().configureEach {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
 }
 
 tasks.register<Delete>("clean") {
