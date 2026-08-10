10a9cf9c0d2c90944a23e283d60628208ad8a411
Ebrahassad <ebrahassadi1@gmail.com>
2026-08-10 21:25:40 +0200

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,40 +1,24 @@
 import 'package:flutter/material.dart';
 
-import 'splash_screen.dart';
-
+import 'features/puzzle/screens/world_map_screen.dart';
 
 class PuzzleWorldApp extends StatelessWidget {
-
   const PuzzleWorldApp({
     super.key,
   });
 
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
-
       title: "Puzzle World",
-
-
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.blue,
         ),
-
         useMaterial3: true,
-
       ),
-
-
-      home: const SplashScreen(),
-
+      home: const WorldMapScreen(),
     );
-
   }
-
 }
